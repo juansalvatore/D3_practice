fe07e4e6c04b3d10703a8a07ba9812b88b9b0de5
Juan Salvatore <juansalvatore@live.com.ar>
2018-01-06 17:44:22 -0300

    Create D3 CSV load function and add generate function

--- D3_fundamentals/main.js
+++ D3_fundamentals/main.js
@@ -1,28 +1,48 @@
-const dataset = [10, 20, 30, 40, 50]
+// const dataset = [10, 20, 30, 40, 50]
+d3.csv('data.csv', (err, data) => {
+  if (err) {
+    return console.log(err)
+  }
+  console.log(data)
+  generate(data.columns)
+})
 
-let el = d3
-  .select('body')
-  .selectAll('p')
-  .data(dataset)
-  .enter()
-  .append('p')
-  .text(data => {
-    return 'This paragraph is binded to the number ' + data
-  })
-  // .append('p')
-  // .attr('class', 'foo')
-  // .attr('class', 'bar')
-  // .classed('foo', true)
-  // .classed('bar', true)
-  // .text('Hello World')
-  .style('color', data => {
-    if (data > 25) {
-      return 'red'
-    } else {
-      return 'blue'
-    }
-  })
+generate = dataset => {
+  let el = d3
+    .select('body')
+    .selectAll('p')
+    .data(dataset)
+    .enter()
+    .append('p')
+    .text(data => {
+      return 'This paragraph is binded to the number ' + data
+    })
+    .attr('class', data => {
+      switch (data) {
+        case 10:
+          return 'lowest'
+          break
+        case 50:
+          return 'highest'
+          break
+        default:
+          return 'normal'
+          break
+      }
+    })
+    // .attr('class', 'bar')
+    .classed('lessThanTwentyFive', data => {
+      return data < 25
+    })
+    .style('color', data => {
+      if (data > 25) {
+        return 'red'
+      } else {
+        return 'blue'
+      }
+    })
 
-console.log(el)
+  console.log(el)
 
-// selection methods (.select, .append), transformation methods (.attr, .text)
+  // selection methods (.select, .append), transformation methods (.attr, .text)
+}
